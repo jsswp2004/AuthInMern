fd3dd8281e6a2fbea2acfd36a7a089fb4d96e840
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-01-03 21:04:34 -0500

    cc

--- server/db.js
+++ server/db.js
@@ -6,6 +6,7 @@ module.exports = () => {
 		useUnifiedTopology: true,
 	};
 	try {
+		mongoose.set('strictQuery', false);
 		mongoose.connect(process.env.DB, connectionParams);
 		console.log("Connected to database successfully");
 	} catch (error) {
