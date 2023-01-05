c8fb1eede712659fb894c898343ec677abc77f1a
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-01-04 20:58:28 -0500

    cc

--- client/src/components/Main/mainIndex.jsx
+++ client/src/components/Main/mainIndex.jsx
@@ -83,13 +83,44 @@ const Main = () => {
   // )
 
   // This method will delete a record
-  async function deleteRecord(id) {
-    await fetch(`http://localhost:8080/userlist/${id}`, {
-      method: 'DELETE',
-    })
+  // async function deleteRecord(id) {
+  //   await fetch(`http://localhost:8080/userlist/${id}`, {
+  //     method: 'DELETE',
+  //   })
 
-    const newRecords = userRecords.filter((el) => el._id !== id)
-    setUsersRecords(newRecords)
+  //   const newRecords = userRecords.filter((el) => el._id !== id)
+  //   setUsersRecords(newRecords)
+  // }
+
+  // async function deleteRecord(id) {
+  //   await fetch(`http://localhost:8080/userlist/${id}`, {
+  //     method: 'DELETE',
+  //   })
+
+  //   const newRecords = userRecords.filter((el) => el._id !== id)
+  //   setUsersRecords(newRecords)
+  // }
+
+  // async function deleteRecord(id) {
+  //   const url = 'http://localhost:8080/api/userlist'
+  //   const { data: res } = await axios.get(url, data)
+
+  //   const userList = res.data
+  //   setUsersRecords(userList)
+  //   // window.location = '/'
+  // }
+
+  async function deleteRecord(id) {
+    axios.delete(`http://localhost:8080/api/userlist/${id}`)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    
+      const newRecords = userRecords.filter((el) => el._id !== id)
+      setUsersRecords(newRecords)
   }
 
   // This method will map out the records on the table
@@ -110,22 +141,22 @@ const Main = () => {
     
       <div className="grid-container">
         <div className="item1">
-          <nav className="navbar">
+          {/* <nav className="navbar"> */}
             <div className="header_logo">
               <img src={logo} className="App_logo" alt="poehr" />
               <h3>POEHR</h3>
             </div>
 
-            <button className="btn btn-danger btn-sm" onClick={handleLogout}>
+            <button className="btn btn-danger btn-sm btn-rounded" onClick={handleLogout}>
               {/* Logout */}
               {/* <LogoutIcon className="tooltip" >
                 
             </LogoutIcon> */}
-            <i class="fa fa-sign-out fa-sm" aria-hidden="true">
-              <span className="tooltiptext">Exit</span>
+            <i className="fa fa-sign-out fa-lg" aria-hidden="true" title='Exit'>
+              {/* <span className="tooltiptext">Exit</span> */}
               </i>
             </button>
-          </nav>
+          {/* </nav> */}
         </div>
         <div className="item2"></div>
 
