834ecfea144c3faec50d5b8b3c745d3c5181c2f6
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-01-05 01:31:40 -0500

    cc

---
+++ client/src/components/Navigation/navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+// import 'bootstrap/dist/css/bootstrap.css'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div className="header-bodynavbar">
+      <div className="body-navbars" >
+        <ul className="body-navbar">
+          <li className="body-navbarlist">
+            <Link to="/">Patient List</Link>
+          </li>
+          <li className="body-navbarlist">
+            <Link to="/create">Registration</Link>
+          </li>
+          <li className="body-navbarlist">
+            <Link to="/visitList">Schedule</Link>
+          </li>
+          <li className="body-navbarlist">
+            <Link to="/usersList">User List</Link>
+          </li>
+        </ul>
+        <div>
+        </div>
+      </div>
+    </div>
+  )
+}
