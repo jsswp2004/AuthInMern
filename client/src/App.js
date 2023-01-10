70654e42de9172d09ac77f549b805998b6f32f40
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-01-10 16:55:10 -0500

    cc

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import ShowUsersList from './components/Signup/showUsersList'
 import EditUser from './components/Signup/editUser'
 import CreateUser from './components/Signup/createUser'
 import CreateClient from './components/PatientRegistration/createClient'
+import PatientList from './components/PatientRegistration/ShowPatientList'
 
 function App() {
   const user = localStorage.getItem('token')
@@ -16,6 +17,7 @@ function App() {
       {user && <Route path="/" exact element={<Main />} />}
       <Route path="/signup" exact element={<Signup />} />
       <Route path="/createClient" exact element={<CreateClient />} />
+      <Route path="/patientlist" exact element={<PatientList />} />      
       <Route path="/createUser" exact element={<CreateUser />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/usersList" exact element={<ShowUsersList />} />
