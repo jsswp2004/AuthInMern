69fa21ab5e1935e331a607e430438ab29d50e96b
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-01-18 14:29:54 -0500

    cleaned up calendar

--- client/src/components/Scheduling/visitWeekly.js
+++ client/src/components/Scheduling/visitWeekly.js
@@ -5,55 +5,32 @@ import { textAlign } from '@mui/system'
 const VisitWeekly = (props) => {
   return (
     <>
-      {/* visitDate={props.visit.visitDate} */}
       <tr
         className="trStyles"
-        style={{ display: 'flex', paddingBottom: '0px', paddingTop: '0px'}}
+        
       >
-        {/* style={{ display: 'flex' }} */}
-        {/* <td className="weeklytd">{props.visit.visitDate}</td> */}
-        {/* <div> */}
-          {/* style={{ float: 'left' }} */}
-          <td className="weeklytd">{props.visit.hourOfVisit}</td>
-        {/* </div> */}
-        {/* <div> */}
-          <td className="weeklytd">{props.visit.firstName}</td>
-        {/* </div> */}
-        {/* <div> */}
-          <td className="weeklytd">{props.visit.lastName}</td>
-        {/* </div> */}
-
-        {/* <div style={{ float: 'right' }}> */}
-          {/* <Link
-            to={`/editVisit/${props.visit._id}`}
-          >
-            <i
-              className="fa fa-pencil-square-o fa-sm"
-              aria-hidden="true"
-              style={{ color: 'blue', paddingTop: '5px' }}
-            />
-          </Link>{' '} */}
-          <td className="weeklytd">
-          <Link
-            to={`/editVisit/${props.visit._id}`}
-          >
+        {/* style={{ display: 'flex', paddingBottom: '0px', paddingTop: '0px' }} */}
+        <td style={{ paddingLeft: '2px', paddingRight: '2px', paddingBottom: '0px', paddingTop: '2px' }} className="weeklytd">{props.visit.hourOfVisit}</td>
+        <td style={{  paddingLeft: '2px', paddingRight: '2px',  paddingBottom: '0px', paddingTop: '2px' }} className="weeklytd">{props.visit.firstName}</td>
+        <td style={{  paddingLeft: '2px', paddingRight: '2px',  paddingBottom: '0px', paddingTop: '2px' }} className="weeklytd">{props.visit.lastName}</td>
+        <td style={{  paddingLeft: '4px', paddingRight: '2px',  paddingBottom: '0px', paddingTop: '0px' }} className="weeklytd">
+          <Link to={`/editVisit/${props.visit._id}`}>
             <i
               className="fa fa-pencil-square-o fa-sm"
               aria-hidden="true"
               style={{ color: 'blue', paddingTop: '5px' }}
             />
           </Link>{' '}
-            <i
-              className="fa fa-trash fa-sm"
-              aria-hidden="true"
-              onClick={() => {
-                props.deleteRecord(props.visit._id)
-              }}
-              style={{ color: 'red', paddingTop: '5px' }}
-              title="Delete"
-            />
-          </td>
-        {/* </div> */}
+          <i
+            className="fa fa-trash fa-sm"
+            aria-hidden="true"
+            onClick={() => {
+              props.deleteRecord(props.visit._id)
+            }}
+            style={{ color: 'red', paddingTop: '5px' }}
+            title="Delete"
+          />
+        </td>
       </tr>
     </>
   )
