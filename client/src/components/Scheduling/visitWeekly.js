2640ee59dcf104c966eb2a7b1fa37b013ad225dc
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-01-19 07:33:59 -0500

    cc

--- client/src/components/Scheduling/visitWeekly.js
+++ client/src/components/Scheduling/visitWeekly.js
@@ -6,14 +6,13 @@ const VisitWeekly = (props) => {
   return (
     <>
       <tr
-        className="trStyles"
-        
+        className="trStyles"       
       >
         {/* style={{ display: 'flex', paddingBottom: '0px', paddingTop: '0px' }} */}
-        <td style={{ paddingLeft: '2px', paddingRight: '2px', paddingBottom: '0px', paddingTop: '2px' }} className="weeklytd">{props.visit.hourOfVisit}</td>
-        <td style={{  paddingLeft: '2px', paddingRight: '2px',  paddingBottom: '0px', paddingTop: '2px' }} className="weeklytd">{props.visit.firstName}</td>
-        <td style={{  paddingLeft: '2px', paddingRight: '2px',  paddingBottom: '0px', paddingTop: '2px' }} className="weeklytd">{props.visit.lastName}</td>
-        <td style={{  paddingLeft: '4px', paddingRight: '2px',  paddingBottom: '0px', paddingTop: '0px' }} className="weeklytd">
+        <td style={{ width:'15%',paddingLeft: '2px', paddingRight: '2px', paddingBottom: '0px', paddingTop: '2px' }} className="weeklytd">{props.visit.hourOfVisit}</td>
+        <td style={{width:'35%', paddingLeft: '2px', paddingRight: '2px',  paddingBottom: '0px', paddingTop: '2px' }} className="weeklytd">{props.visit.firstName}</td>
+        <td style={{ width:'35%', paddingLeft: '2px', paddingRight: '2px',  paddingBottom: '0px', paddingTop: '2px' }} className="weeklytd">{props.visit.lastName}</td>
+        <td style={{  width:'15%', paddingLeft: '4px', paddingRight: '2px',  paddingBottom: '0px', paddingTop: '0px' }} className="weeklytd">
           <Link to={`/editVisit/${props.visit._id}`}>
             <i
               className="fa fa-pencil-square-o fa-sm"
