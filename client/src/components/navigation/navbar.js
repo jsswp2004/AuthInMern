25140c17b28dc62dfcb4fe8f1908e02fa4d5903a
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-01-19 12:47:15 -0500

    cc

--- client/src/components/navigation/navbar.js
+++ client/src/components/navigation/navbar.js
@@ -11,9 +11,9 @@ export default function Navbar() {
     <div id="navbarSupportedContent">
       <div className="header-bodynavbar">
         <ul className="body_navbar">
-          <li id="link" className="body_navbarlist">
+          {/* <li id="link" className="body_navbarlist">
             <Link to="/">Home</Link>
-          </li>
+          </li> */}
           <li id="link" className="body_navbarlist">
             <Link to="/patientlist">Patient List</Link>
           </li>
@@ -21,7 +21,7 @@ export default function Navbar() {
             <Link to="/createClient">Registration</Link>
           </li> */}
           <li id="link" className="body_navbarlist">
-            <Link to="/createPatient">Register Patient</Link>
+            <Link to="/createPatient">Patient Registration</Link>
           </li>
           {/* <li id="link" className="body_navbarlist">
             <Link to="/visitlist">Schedule</Link>
@@ -32,12 +32,20 @@ export default function Navbar() {
           {/* <li id="link" className="body_navbarlist">
             <Link to="/createVisit">Create Schedule</Link>
           </li> */}
-          <li id="link" className="body_navbarlist">
+
+          {/* <li id="link" className="body_navbarlist">
             <Link to="/usersList">User List</Link>
-          </li>
+          </li> */}
           {/* <li id="link" className="body_navbarlist">
             <Link to="/createUser">Create User</Link>
           </li> */}
+
+          <li class="body_navbarlist dropdown">
+            <span>Settings</span>
+            <div class="dropdown-content">
+              <a href="/usersList">User Profiles</a>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
