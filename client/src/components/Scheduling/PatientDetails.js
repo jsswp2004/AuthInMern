5a6563537d6a51664ac0bbfb8180dfd139447cb9
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-01-24 22:14:59 -0500

    cc

--- client/src/components/Scheduling/PatientDetails.js
+++ client/src/components/Scheduling/PatientDetails.js
@@ -6,24 +6,82 @@ import {
   View,
   StyleSheet,
   PDFViewer,
+  Font,
 } from '@react-pdf/renderer'
+
+// Register font
+
+Font.register({ family: 'Helvetica' })
+Font.register({
+  family: 'Oswald',
+  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf',
+})
 // Create styles
+
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     backgroundColor: 'white',
   },
   section: {
+    flexDirection: 'column',
     margin: 10,
     padding: 10,
     flexGrow: 1,
   },
-  // Text: {
-  //   fontSize: 8,
-  // },
+  section1: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
   viewer: {
     width: '100%', //window.innerWidth, //the pdf viewer will take up all of the width and height
     height: '880px',
+  },
+  body: {
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontSize: 16,
+    textAlign: 'center',
+    fontFamily: 'Oswald',
+  },
+  author: {
+    fontSize: 12,
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  subtitle: {
+    fontSize: 12,
+    margin: 1,
+    fontFamily: 'Oswald',
+  },
+  text: {
+    margin: 1,
+    fontSize: 12,
+    textAlign: 'justify',
+    fontFamily: 'Times-Roman',
+  },
+  image: {
+    marginVertical: 15,
+    marginHorizontal: 100,
+  },
+  header: {
+    fontSize: 12,
+    marginBottom: 12,
+    textAlign: 'left',
+    color: 'grey',
+  },
+  pageNumber: {
+    position: 'absolute',
+    fontSize: 12,
+    bottom: 30,
+    left: 0,
+    right: 0,
+    textAlign: 'center',
+    color: 'grey',
   },
 })
 
@@ -37,65 +95,44 @@ const PatientDetails = (props) => {
         {/*render a single page*/}
         <Page size="A4" style={styles.page}>
           <View style={styles.section}>
-            
-              <Text
-                style={{ fontSize: '10px' }}
-              >
-                Patient Visit Details
+            <Text style={styles.header} fixed>
+              POEHR&#174;
+            </Text>
+            <Text style={styles.title}>Patient Visit Details</Text>
+
+            <View style={styles.section1}>
+              <Text style={styles.subtitle}>Patient Name:</Text>
+              <Text style={styles.text}>
+                {props.visit.firstName} {props.visit.middleName}{' '}
+                {props.visit.lastName}
               </Text>
-            
-            <Text style={{ fontSize: '10px' }}>
-              Patient Name: {props.visit.firstName} {props.visit.middleName}{' '}
-              {props.visit.lastName}
-            </Text>
-            <Text
-                style={{ fontSize: '10px' }}
-              >
-                Email
+              <Text style={styles.subtitle}>Email:</Text>
+              <Text style={styles.text}>{props.visit.email}</Text>
+              <Text style={styles.subtitle}>Visit Date:</Text>
+              <Text style={styles.text}>
+                {props.visit.visitDate} {props.visit.hourOfVisit}
               </Text>
-            
-            <Text style={{ fontSize: '10px' }}>
-              Email: {props.visit.email}
-            </Text>
-          </View>
-          <View style={styles.section}>
-            <Text style={{ fontSize: '10px' }}>Appointment Date and Time</Text>
-            <Text style={{ fontSize: '10px' }}>
-              Visit Date: {props.visit.visitDate} {props.visit.hourOfVisit}
-            </Text>
-            <Text style={{ fontSize: '10px' }}>Provider</Text>
-            <Text style={{ fontSize: '10px' }}>
-              Physician: {props.visit.provider} 
-            </Text>
+              <Text style={styles.subtitle}>Physician:</Text>
+              <Text style={styles.text}>{props.visit.provider}</Text>
+            </View>
+            {/* <View style={styles.section}>
+              <Text style={styles.subtitle}>Patient Name:</Text>
+              <Text style={styles.text}>
+                {props.visit.firstName} {props.visit.middleName}{' '}
+                {props.visit.lastName}
+              </Text>
+              <Text style={styles.subtitle}>Email:</Text>
+              <Text style={styles.text}>{props.visit.email}</Text>
+              <Text style={styles.subtitle}>Visit Date:</Text>
+              <Text style={styles.text}>
+                {props.visit.visitDate} {props.visit.hourOfVisit}
+              </Text>
+              <Text style={styles.subtitle}>Physician:</Text>
+              <Text style={styles.text}>{props.visit.provider}</Text>
+            </View> */}
           </View>
         </Page>
       </Document>
-      {/* <div className="patientDetails">
-      <div className="patientDetailsDemographics">
-        {' '}
-        <div>
-          <h6>Patient Name </h6>
-          {props.visit.firstName} {props.visit.middleName}{' '}
-          {props.visit.lastName}
-        </div>{' '}
-        <div>
-          <h6>Email</h6>
-          {props.visit.email}
-        </div>
-      </div>
-      <div className="patientDetailsDemographics">
-        {' '}
-        <div>
-          <h6>Appointment Date & Time </h6>
-          {props.visit.visitDate} {props.visit.hourOfVisit}
-        </div>
-        <div>
-          <h6>Provider</h6>
-          {props.visit.provider}
-        </div>
-      </div>
-      <div className="patientDetailsDemographics"></div>
-    </div> */}
     </PDFViewer>
   )
 }
