cdbe9bc5f2f5fb26fb403e7a8335462065ef1c6c
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-02-13 01:39:42 -0500

    cc

--- client/src/components/Main/About.js
+++ client/src/components/Main/About.js
@@ -19,15 +19,21 @@ const Main = () => {
         <Navbar />
       </div> */}
       <div className="item2About">
-        <div className="HomepageGreeting">
+        {/* <div className="HomepageGreeting"> */}
           <div className="aboutPage1">
             {/* style={{ backgroundImage: URL('./images/aboutPageDoc.jpg')}} */}
-            <h3 className="aboutPageTitle1">We Are POEHR</h3>
+            <h3 className="aboutPageTitle1">We Are POEHR.</h3>
 
-            <h5 className="aboutPageTitle2">Our company, POEHR, Inc., is dedicated to revolutionizing the way
+            <h5 className="aboutPageTitle2">Dedicated to revolutionizing the way
               healthcare providers store and access patient information. </h5>
-            <p className="aboutPageDescription">
-              We understand the importance of accurate and up-to-date medical
+            {/* <p className="aboutPageDescription">
+ 
+            </p> */}
+            {/* <img src={AboutImg} className="About_img" alt="about" /> */}
+          </div>
+          <div className="aboutPage1"> 
+          <div className="aboutPage2 aboutPageDescription">
+          At POEHR, we understand the importance of accurate and up-to-date medical
               records, and our state-of-the-art electronic medical record system
               tools are designed to streamline the process for both medical
               professionals and patients. Our system is fully compliant with all
@@ -36,13 +42,7 @@ const Main = () => {
               information, reducing the need for paper records and minimizing
               errors. In addition to our electronic medical record system, we
               also offer training and support to ensure that healthcare
-              providers are able to fully utilize our systems' features. 
-            </p>
-            {/* <img src={AboutImg} className="About_img" alt="about" /> */}
-          </div>
-          {/* <div> */}
-          <div className="aboutPage2">
-We are constantly updating our
+              providers are able to fully utilize our systems' features. We are constantly updating our
             software solutions to stay current with the latest advancements in
             technology, and we are committed to providing the best possible
             service to our clients. If you're ready to take the next step in
@@ -53,9 +53,9 @@ We are constantly updating our
           <div className="aboutPage3">
             <img src={AboutImg} className="About_img" alt="about" />
           </div>
-          {/* </div> */}
+          </div> 
 
-        </div>
+        {/* </div> */}
       </div>
     </div>
   )
