436324406a5cb9b4fdb6527f8f35a7ffe2c7b8bc
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-02-13 15:36:31 -0500

    cc

--- client/src/components/Main/About.js
+++ client/src/components/Main/About.js
@@ -20,29 +20,31 @@ const Main = () => {
       </div> */}
       <div className="item2About">
         {/* <div className="HomepageGreeting"> */}
-          <div className="aboutPage1">
-            {/* style={{ backgroundImage: URL('./images/aboutPageDoc.jpg')}} */}
-            <h3 className="aboutPageTitle1">We Are POEHR.</h3>
+        <div className="aboutPage1">
+          {/* style={{ backgroundImage: URL('./images/aboutPageDoc.jpg')}} */}
+          <h3 className="aboutPageTitle1">We Are POEHR.</h3>
 
-            <h5 className="aboutPageTitle2">Dedicated to revolutionizing the way
-              healthcare providers store and access patient information. </h5>
-            {/* <p className="aboutPageDescription">
+          <h5 className="aboutPageTitle2">
+            Dedicated to revolutionizing the way healthcare providers store and
+            access patient information.{' '}
+          </h5>
+          {/* <p className="aboutPageDescription">
  
             </p> */}
-            {/* <img src={AboutImg} className="About_img" alt="about" /> */}
-          </div>
-          <div className="aboutPage1"> 
+          {/* <img src={AboutImg} className="About_img" alt="about" /> */}
+        </div>
+        <div className="aboutPage1">
           <aside className="aboutPage2 aboutPageDescription">
-          At POEHR, we understand the importance of accurate and up-to-date medical
-              records, and our state-of-the-art electronic medical record system
-              tools are designed to streamline the process for both medical
-              professionals and patients. Our system is fully compliant with all
-              relevant industry regulations, including HIPAA and HITECH. It
-              allows for secure, electronic storage and sharing of patient
-              information, reducing the need for paper records and minimizing
-              errors. In addition to our electronic medical record system, we
-              also offer training and support to ensure that healthcare
-              providers are able to fully utilize our systems' features. We are constantly updating our
+            At POEHR, we understand the importance of accurate and up-to-date
+            medical records, and our state-of-the-art electronic medical record
+            system tools are designed to streamline the process for both medical
+            professionals and patients. Our system is fully compliant with all
+            relevant industry regulations, including HIPAA and HITECH. It allows
+            for secure, electronic storage and sharing of patient information,
+            reducing the need for paper records and minimizing errors. In
+            addition to our electronic medical record system, we also offer
+            training and support to ensure that healthcare providers are able to
+            fully utilize our systems' features. We are constantly updating our
             software solutions to stay current with the latest advancements in
             technology, and we are committed to providing the best possible
             service to our clients. If you're ready to take the next step in
@@ -53,7 +55,7 @@ const Main = () => {
           <aside className="aboutPage3">
             <img src={AboutImg} className="About_img" alt="about" />
           </aside>
-          </div> 
+        </div>
 
         {/* </div> */}
       </div>
