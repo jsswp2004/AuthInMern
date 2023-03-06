261ee3a95393260b384a43cd35c0a9d8dbac2259
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-03-06 18:00:35 -0500

    cc

--- client/src/components/Scheduling/createVisitFromRegModal.js
+++ client/src/components/Scheduling/createVisitFromRegModal.js
@@ -9,6 +9,7 @@ import { Hour } from '../listDictionaries/listData/listDictionariesData'
 
 const CreateVisitFromReg = (props) => {
   
+  const [selectedHour, setSelectedHour] = useState('')
 
   const navigate = useNavigate()
   const [record, setRecord] = useState({
@@ -67,7 +68,7 @@ const CreateVisitFromReg = (props) => {
     lastName,
     middleName,
     visitDate,
-    hourOfVisit,
+    // hourOfVisit,
     provider,
     email,
     event,
@@ -82,7 +83,7 @@ const CreateVisitFromReg = (props) => {
     lastName: lastName,
     middleName: middleName,
     visitDate: '',
-    hourOfVisit: '',
+    hourOfVisit: selectedHour,
     email: email,
     provider: '',
     addedDate: format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
@@ -129,6 +130,27 @@ const CreateVisitFromReg = (props) => {
         console.log('Error from event list')
       })
   }, [])
+  const [getVisits, setGetVisits] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:8081/api/visits')
+      .then((response) => {
+        const data = response.data
+        setGetVisits(data)
+      })
+      .catch((error) => {
+        console.log('Error from visit list')
+      })
+  }, [])
+  let { hourOfVisit } = visit
+  hourOfVisit = selectedHour
+  const selectedMD = visit.provider
+  const selectedDate = visit.visitDate
+  const filteredVisitsWithMD = getVisits.filter((visit) => {
+    return visit.provider === selectedMD && visit.visitDate === selectedDate
+  })
+  const filteredVisitsWithMDAndDate = filteredVisitsWithMD.map((doc) => doc.hourOfVisit)
+
 
   const onSubmit = (e) => {
     e.preventDefault()
@@ -147,7 +169,7 @@ const CreateVisitFromReg = (props) => {
           : record.email,
       addedDate: visit.addedDate,
       visitDate: visit.visitDate,
-      hourOfVisit: visit.hourOfVisit,
+      hourOfVisit: hourOfVisit,
       provider: visit.provider,
       event: visit.event,
     }
@@ -208,28 +230,6 @@ const CreateVisitFromReg = (props) => {
                     </label>
                   </div>
                   <div className="form-group">
-                    <label htmlFor="hourOfVisit">
-                      Hour of Visit
-                      <select
-                        key={visit.hourOfVisit}
-                        className="form-control select"
-                        name="hourOfVisit"
-                        value={visit.hourOfVisit}
-                        onChange={onChange}
-                      >
-                        {hourValues.map((hourval) => (
-                          <option key={hourval.value} value={hourval.value}>
-                            {hourval.label}
-                          </option>
-                        ))}
-                      </select>
-                    </label>
-                  </div>
-                </div>
-              </div>
-
-              <div className="div-items">
-                <div className="form-group">
                   <label htmlFor="email">
                     Email
                     <input
@@ -242,76 +242,12 @@ const CreateVisitFromReg = (props) => {
                     />
                   </label>
                 </div>
-                <div className="form-group">
-                  <label htmlFor="addedDate">
-                    Date Created
-                    <input
-                      name="addedDate"
-                      className="form-control"
-                      value={visit.addedDate}
-                      onChange={onChange}
-                    />
-                  </label>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="visitDate">
-                    Visit Date
-                    <input
-                      type="date"
-                      name="visitDate"
-                      className="form-control"
-                      value={visit.visitDate}
-                      onChange={onChange}
-                    />
-                  </label>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="provider">
-                    Provider
-                    <select
-                      key={visit.provider}
-                      className="form-control select"
-                      name="provider"
-                      value={visit.provider}
-                      onChange={onChange}
-                    >
-                      {' '}
-                      <option key="0" value="Select Provider">
-                        Select Provider
-                      </option>
-                      {providerMD.map((doc) => (
-                        <option key={doc._id} value={doc.provider}>
-                          {doc}
-                        </option>
-                      ))}
-                    </select>
-                  </label>
+
                 </div>
               </div>
-              <div className="div-items updateRegistrationGrp">
-                <div className="form-group">
-                  <label htmlFor="event">
-                    Scheduled Event
-                    <select
-                      key={visit.event._id}
-                      className="form-control select"
-                      name="event"
-                      value={visit.event}
-                      onChange={onChange}
-                    >
-                      {' '}
-                      <option key="0" value="Select Event">
-                        Select Event
-                      </option>
-                      {clinicEvents.map((doc) => (
-                        <option key={doc._id} value={doc.name}>
-                          {doc}
-                        </option>
-                      ))}
-                    </select>
-                  </label>
-                </div>
-                <div className="form-group">
+
+              <div className="div-items">
+              <div className="form-group">
                   <label htmlFor="medicalRecordNumber">
                     MRN
                     <input
@@ -339,6 +275,141 @@ const CreateVisitFromReg = (props) => {
                     />
                   </label>
                 </div>
+                <div className="form-group">
+                  <label htmlFor="addedDate">
+                    Date Created
+                    <input
+                      name="addedDate"
+                      className="form-control"
+                      value={visit.addedDate}
+                      onChange={onChange}
+                    />
+                  </label>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="visitDate">
+                    Visit Date
+                    <input
+                      type="date"
+                      name="visitDate"
+                      className="form-control"
+                      value={visit.visitDate}
+                      onChange={onChange}
+                    />
+                  </label>
+                </div>
+
+              </div>
+              <div className="div-items updateRegistrationGrp">
+              <div className="form-group">
+                  <label htmlFor="provider">
+                    Provider
+                    <select
+                      key={visit.provider}
+                      className="form-control select"
+                      name="provider"
+                      value={visit.provider}
+                      onChange={onChange}
+                    >
+                      {' '}
+                      <option key="0" value="Select Provider">
+                        Select Provider
+                      </option>
+                      {providerMD.map((doc) => (
+                        <option key={doc._id} value={doc.provider}>
+                          {doc}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="event">
+                    Scheduled Event
+                    <select
+                      key={visit.event._id}
+                      className="form-control select"
+                      name="event"
+                      value={visit.event}
+                      onChange={onChange}
+                    >
+                      {' '}
+                      <option key="0" value="Select Event">
+                        Select Event
+                      </option>
+                      {clinicEvents.map((doc) => (
+                        <option key={doc._id} value={doc.name}>
+                          {doc}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="hourOfVisit">
+                      Hour of Visit
+                      <input
+                      type="text"
+                      name="hourOfVisit"
+                      className="form-control"
+                      // value={visit.hourOfVisit}
+                      value={selectedHour}
+                      onChange={onChange}
+                    />
+                    {/* <select
+                      key={visit.hourOfVisit}
+                      className="form-control select"
+                      name="hourOfVisit"
+                      value={visit.hourOfVisit}
+                      onChange={onChange}
+                    >
+                      {hourValues.map((hourval) => (
+                        <option key={hourval.value} value={hourval.value}>
+                          {hourval.label}
+                        </option>
+                      ))}
+                    </select> */}
+                    <div className='hour-flex'>
+
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('09:00')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('09:00') ? '#AA336A' : '#90EE90' }}>9:00</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('09:15')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('09:15') ? '#AA336A' : '#90EE90' }}>9:15</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('09:30')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('09:30') ? '#AA336A' : '#90EE90' }}>9:30</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('09:45')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('09:45') ? '#AA336A' : '#90EE90' }}>9:45</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('10:00')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('10:00') ? '#AA336A' : '#90EE90' }}>10:00</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('10:15')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('10:15') ? '#AA336A' : '#90EE90' }}>10:15</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('10:30')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('10:30') ? '#AA336A' : '#90EE90' }}>10:30</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('10:45')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('10:45') ? '#AA336A' : '#90EE90' }}>10:45</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('11:00')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('11:00') ? '#AA336A' : '#90EE90' }}>11:00</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('11:15')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('11:15') ? '#AA336A' : '#90EE90' }}>11:15</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('11:30')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('11:30') ? '#AA336A' : '#90EE90' }}>11:30</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('11:45')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('11:45') ? '#AA336A' : '#90EE90' }}>11:45</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('12:00')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('12:00') ? '#AA336A' : '#90EE90' }}>12:00</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('12:15')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('12:15') ? '#AA336A' : '#90EE90' }}>12:15</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('12:30')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('12:30') ? '#AA336A' : '#90EE90' }}>12:30</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('12:45')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('12:45') ? '#AA336A' : '#90EE90' }}>12:45</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('13:00')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('13:00') ? '#AA336A' : '#90EE90' }}>13:00</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('13:15')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('13:15') ? '#AA336A' : '#90EE90' }}>13:15</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('13:30')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('13:30') ? '#AA336A' : '#90EE90' }}>13:30</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('13:45')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('13:45') ? '#AA336A' : '#90EE90' }}>13:45</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('14:00')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('14:00') ? '#AA336A' : '#90EE90' }}>14:00</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('14:15')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('14:15') ? '#AA336A' : '#90EE90' }}>14:15</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('14:30')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('14:30') ? '#AA336A' : '#90EE90' }}>14:30</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('14:45')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('14:45') ? '#AA336A' : '#90EE90' }}>14:45</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('15:00')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('15:00') ? '#AA336A' : '#90EE90' }}>15:00</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('15:15')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('15:15') ? '#AA336A' : '#90EE90' }}>15:15</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('15:30')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('15:30') ? '#AA336A' : '#90EE90' }}>15:30</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('15:45')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('15:45') ? '#AA336A' : '#90EE90' }}>15:45</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('16:00')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('16:00') ? '#AA336A' : '#90EE90' }}>16:00</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('16:15')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('16:15') ? '#AA336A' : '#90EE90' }}>16:15</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('16:30')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('16:30') ? '#AA336A' : '#90EE90' }}>16:30</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('16:45')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('16:45') ? '#AA336A' : '#90EE90' }}>16:45</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('17:00')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('17:00') ? '#AA336A' : '#90EE90' }}>17:00</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('17:15')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('17:15') ? '#AA336A' : '#90EE90' }}>17:15</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('17:30')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('17:30') ? '#AA336A' : '#90EE90' }}>17:30</div>
+                      <div className='hour-flex_Item' onClick={() => setSelectedHour('17:45')} style={{ backgroundColor: filteredVisitsWithMDAndDate.includes('17:45') ? '#AA336A' : '#90EE90' }}>17:45</div>
+                    </div>
+                    </label>
+                  </div>
                 <div
                   className="form-group"
                   style={{
