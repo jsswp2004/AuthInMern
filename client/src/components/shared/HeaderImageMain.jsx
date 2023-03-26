4bee1db05fae210274a41c5def83a881cff340c5
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-03-26 19:43:34 -0400

    cc

--- client/src/components/shared/HeaderImageMain.jsx
+++ client/src/components/shared/HeaderImageMain.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import { FirstNameContext } from '../../App'
 import logo from './images/logoPOWER.png'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import { useContext } from 'react'
-import { width } from '@mui/system'
-// import { UserContext } from '../contexts/user.context'
-// import LogoutIcon from '@mui/icons-material/Logout'
-// create context
-// const UserContext = createContext();
-// import Home from "../pages/Home.page"
+import { Link, useNavigate } from 'react-router-dom'
 function HeaderImageMain() {
 
-  // const userFirstName = useContext(FirstNameContext);
   let navigate = useNavigate()
   const toLogin = () => {
     localStorage.removeItem('token')
@@ -25,25 +16,15 @@ function HeaderImageMain() {
 
   return (
     <nav className="navbar1">
-      {/* <div className="header"> */}
       <div className="headerItem">
         <img src={logo} className="App_logo" alt="logo" />{' '}
         <h3 className="h3header">
           POEHR
         </h3>
-        {/* <h5 className="h3header">
-            Welcome {userFirstName}!
-          </h5> */}
+
       </div>
 
       <ul className="navigation">
-        {/* <span className="navbar_listHeaderGreeting">
-          Welcome {userFirstName}!
-        </span> */}
-        {/* <li id="link" className="navbar_list">
-            <Link to="/createPatient">Registration</Link>
-          </li> */}
-
         <li id="link" className="navbar_list">
           <Link to="/">Home</Link>
         </li>
@@ -51,9 +32,6 @@ function HeaderImageMain() {
         <li id="link" className="navbar_list">
           <Link to="/">Pricing</Link>
         </li>
-        {/* <li id="link" className="navbar_list">
-            <Link to="/contact">Contact</Link>
-          </li> */}
         <li id="link" className="navbar_list">
           <Link to="/about">About us</Link>
         </li>
@@ -62,24 +40,13 @@ function HeaderImageMain() {
           <Link to="/about">Contact</Link>
         </li>
         <button className='mainHeaderBtn btn-info btn navbar_list' onClick={toLogin} style={{ padding: '0', width: '100px', margin: '5px' }}>
-          {/* <li id="link" className="navbar_list"> */}
-          {/* <Link to="/login">Login</Link> */}
-          {/* </li> */}
           Login
         </button>
         <button className='btn btn-success mainHeaderBtn' onClick={toSignup} style={{ padding: '0', width: '100px', margin: '5px' }}>
-
           Signup
         </button>
-        {/* <li> */}
-        {/* <LogoutIcon onClick={handleLogout}>Log Out</LogoutIcon> */}
-        {/* <button className="navbar_list_exit" onClick={handleLogout}>
-          <i className='fa fa-sign-out fa-sm' />
-        </button> */}
-        {/* </li> */}
       </ul>
-      {/* </div> */}
-    </nav >
+    </nav>
   )
 }
 
