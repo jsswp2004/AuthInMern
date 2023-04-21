88ebd4976db4765fe579a7c0950033956b2fde67
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2023-04-21 14:08:06 -0400

    cc

--- client/src/components/Management/Events/uploadEventModal.js
+++ client/src/components/Management/Events/uploadEventModal.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router'
-import { format } from 'date-fns'
 
 import axios from 'axios'
 
@@ -19,7 +18,7 @@ const UploadEvent = (props) => {
     e.preventDefault()
 
     // HANDLING FILE AS SENDING FILE INTO BACKEND
-    // if (!isFilePicked) return;
+    if (!isFilePicked) return;
     const formData = new FormData();
     formData.append("name", selectedFile);
     const config = {
@@ -50,6 +49,10 @@ const UploadEvent = (props) => {
       .post('http://localhost:8081/api/events', formData, config
       ).then(res => {
         console.log(res);
+        window.location.reload()
+      })
+      .catch((err) => {
+        console.log('Error in UploadEventSchedule!')
       })
     navigate('/settingsPage')
     //   .then((res) => {
@@ -109,78 +112,20 @@ const UploadEvent = (props) => {
       <div className="item3">
 
         <div className="createRoleModalBody">
-          {/* <div className="App">
-            <input type="file" name="file" onChange={onChange} />
-            <div>
-              <button onClick={onSubmit}>Submit</button>
-            </div>
-            {isFilePicked ? (
-              <div>
-                <p>Filename: {selectedFile.name}</p>
-                <p>Filetype: {selectedFile.type}</p>
-                <p>Size in bytes: {selectedFile.size}</p>
-                <p>
-                  lastModifiedDate:{" "}
-                  {selectedFile.lastModifiedDate.toLocaleDateString()}
-                </p>
-              </div>
-            ) : (
-              <div>
-                <p>Select a file</p>
-              </div>
-            )}
-          </div> */}
           <div className="item2and3Conatainer">
             {/* <div className="row"> */}
             <form onSubmit={onSubmit}>
-              <h3>Events Upload</h3>
+              {/* <h3>Events Upload</h3> */}
               <div className="form-group">
-                <input type="file" onChange={onChange} />
-
+                <input className="form-control roleInput" type="file" onChange={onChange} />
               </div>
-              {console.log(setSelectedFile)}
+              {/* {console.log(setSelectedFile)} */}
               <div className="form-group">
                 <button className="btn btn-primary" type="submit">Upload</button>
               </div>
             </form>
           </div>
         </div>
-
-        {/* <p>Use the form below to upload a list of roles.
-            Click <a href="/template">here</a> for an example template.</p> */}
-        {/* <form noValidate onSubmit={onSubmit}>
-            <div className="form-group">
-              <label htmlFor="name">Role
-                <input
-                  type="text"
-                  className="form-control roleInput"
-                  name="name"
-                  value={role.name}
-                  onChange={onChange}
-                />
-              </label>
-              <div className="form-group">
-                <label htmlFor="addedDate">Date Created
-                  <input
-                    type="text"
-                    className="form-control roleInput"
-                    name="addedDate"
-                    value={role.addedDate}
-                    onChange={onChange}
-                  />
-                  <input value="Add" type="submit" className="btn btn-success updateRegistrationBtn" />
-                </label>
-              </div>
-            </div>
-          </form> */}
-        {/* <form action="/" method="POST" encType="multipart/form-data">
-
-            <label for="roleName">
-              <input className="form-control roleInput" type="file" id="roleName" name="file" accept="*.csv" />
-            </label>
-            <br /><br />
-            <input className='btn btn-info' type="submit" value="Upload Authors" />
-          </form> */}
       </div>
       {/* </div> */}
     </div>
