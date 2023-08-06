e32eff77b5fa7c5bedc768b33a30edd6d00a999b
Jesus Salvacion <jsswp2004@outlook>
2023-08-06 00:58:46 -0400

    cc

--- client/src/components/Stripe/CardSection.jsx
+++ client/src/components/Stripe/CardSection.jsx
@@ -26,7 +26,7 @@ const CARD_ELEMENT_OPTIONS = {
 function CardSection() {
     return (
         <label>
-            Card details
+            Card Information
             <CardElement options={CARD_ELEMENT_OPTIONS} />
         </label>
     );
